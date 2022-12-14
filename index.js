0a4171ca7f0f553e3ec425c6885cf7e8fb47a365
unknown <j.rosimo098.com>
2022-12-14 19:35:39 +0800

    Feat: add kondisyon api for deployment

---
+++ index.js
@@ -0,0 +1,36 @@
+const { MONGO_URI } = require("./config/keys");
+
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const server = express();
+const port = process.env.RENDER_API || 8099;
+
+//middleWare
+server.use(morgan("dev"));
+server.use(cors());
+server.use(bodyParser.json());
+server.use(helmet());
+
+//routes
+const userRoute = require("./routes/user");
+const usertRoute = require("./routes/usert");
+const adminRoute = require("./routes/admin");
+
+// Database connection
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//Routes
+server.use("/api/v1/user", userRoute);
+server.use("/api/v1/usert", usertRoute);
+server.use("/api/v1/admin", adminRoute);
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
